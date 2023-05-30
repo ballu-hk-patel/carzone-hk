8a0d68b42b31e5bc2768f0f4e84150f6df06b119
ballu-hk-patel <hardikpatel775533@gmail.com>
2023-05-30 14:34:19 +0100

    done

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('',views.home,name='home'),
     path('about',views.about,name='about'),
     path('contanct',views.contanct,name='contanct'),
     path('service',views.service,name='service'),
-]
+] +static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
