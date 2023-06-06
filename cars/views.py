02f7409cd326bb9652d4ee3e550609e0e0215401
ballu-hk-patel <hardikpatel775533@gmail.com>
2023-06-06 12:44:01 +0100

    sec_9_com

--- cars/views.py
+++ cars/views.py
@@ -1,5 +1,100 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import car
+from django.core.paginator import EmptyPage, PageNotAnInteger,Paginator
 # Create your views here.
 def cars(request):
-    return render(request, 'cars/cars.html')
+    cars=car.objects.order_by('-created_date')
+    paginator=Paginator(cars,4)
+    page= request.GET.get('page')
+    paged_cars=paginator.get_page(page)
+
+    model_search=car.objects.values_list('model',flat=True).distinct() # this get only unique value for showing in front page 
+    city_search=car.objects.values_list('city',flat=True).distinct()
+    year_search=car.objects.values_list('year',flat=True).distinct()
+    body_style=car.objects.values_list('body_style',flat=True).distinct()
+
+    data={
+        'cars':paged_cars,
+        'model_search':model_search,
+        'city_search':city_search,
+        'year_search':year_search,
+        'body_style':body_style,
+    }
+    return render(request, 'cars/cars.html',data)
+
+def car_details(request,id):
+    single_car= get_object_or_404(car, pk=id)
+    data={"id":id,
+       "single_car": single_car,}
+    return render(request,'cars/car_details.html',data)
+
+def search(request):
+    cars=car.objects.order_by('-created_date')
+    ss=""
+
+    model_search=car.objects.values_list('model',flat=True).distinct() # this get only unique value for showing in front page 
+    city_search=car.objects.values_list('city',flat=True).distinct()
+    year_search=car.objects.values_list('year',flat=True).distinct()
+    body_style=car.objects.values_list('body_style',flat=True).distinct()
+    tra=car.objects.values_list('transmission',flat=True).distinct()
+    # ss=request.GET.get('keyword') # get html tage name using fetching the data
+    if 'keyword' in request.GET:
+        keyword=request.GET['keyword']
+        print(keyword)
+
+        if keyword:
+            count1=cars.filter(car_title__icontains=keyword).count()
+            if int(count1) == 0:
+                ss=keyword+" is not Avilable ,At this time !"
+            if  int(count1) != 0:
+                print("priting...")
+                cars=cars.filter(car_title__icontains=keyword)
+
+    if 'model' in request.GET:
+        model=request.GET['model']
+        print(model)
+        if model:
+            cars=cars.filter(model__iexact=model)
+
+    if 'city' in request.GET:
+        city=request.GET['city']
+        print(city)
+        if city:
+            cars=cars.filter(city__iexact=city)
+
+
+    if 'year' in request.GET:
+        year=request.GET['year']
+        print(year)
+        if year:
+            cars=cars.filter(year__iexact=year)
+
+    if 'body_style' in request.GET:
+        body_style=request.GET['body_style']
+        print(body_style)
+        if body_style:
+            cars=cars.filter(body_style__iexact=body_style)
+       
+    if 'min_price' in request.GET:
+        min_price=request.GET['min_price']
+        max_price=request.GET['max_price']
+        if max_price:
+            cars=cars.filter(price__gte=min_price,price__lte=max_price)
+
+    if 'transmission' in request.GET:
+        transmission=request.GET['transmission']
+        if transmission:
+            cars=cars.filter(transmission__icontains=transmission)
+
+
+    data={
+        'cars':cars,
+        'ss':ss,
+        'model_search':model_search,
+        'city_search':city_search,
+        'year_search':year_search,
+        'body_style':body_style,
+        'tra':tra,
+        
+    }
+    return render(request,'cars/search.html',data)
--- pages/views.py
+++ pages/views.py
@@ -8,6 +8,12 @@ def home(request):
     teams= Team.objects.all()
     featured_car=car.objects.order_by('-created_date').filter(is_featured=True)
     all_car=car.objects.order_by('-created_date')
+    # search_filed= car.objects.values('model','city','year','body_style')
+    model_search=car.objects.values_list('model',flat=True).distinct() # this get only unique value for showing in front page 
+    city_search=car.objects.values_list('city',flat=True).distinct()
+    year_search=car.objects.values_list('year',flat=True).distinct()
+    body_style=car.objects.values_list('body_style',flat=True).distinct()
+
     for i in all_car:
         if i.car_photo and i.car_photo_1 and i.car_photo_2 and i.car_photo_3 and i.car_photo_4 :
             ss.append(4)
@@ -21,7 +27,13 @@ def home(request):
         'teams': teams,
         'featured_car':featured_car,
         'all_car':all_car,
-        'zipped_app_list': zip(all_car, ss)
+        'zipped_app_list': zip(all_car, ss),
+        'model_search':model_search,
+        'city_search':city_search,
+        'year_search':year_search,
+        'body_style':body_style,
+
+
         
     }
     return render(request, 'pages/home.html',data)
@@ -37,6 +49,12 @@ def contanct(request):
     return render(request, 'pages/contact.html')
 
 def service(request):
+    ss=request.GET.get('keyword')
+    cars=car.objects.order_by('-created_date')
+    data={
+        'cars':cars,
+        'ss':ss
+    }
     return render(request, 'pages/service.html')
 
 # def cars(request):
